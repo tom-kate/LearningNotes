0601de929c34a4e57a722c8a3234a3fe819f7c12
tom-kate <baikaixu1@163.com>
2021-02-17 21:46:15 +0800

    垃圾回收相关算法 引用计数算法 可达性分析算法

---
+++ JVM/code/chapter15/src/com/tomkate/java/RefCountGC.java
@@ -0,0 +1,26 @@
+package com.tomkate.java;
+
+/**
+ * 引用计数算法测试
+ * -XX:+PrintGCDetails
+ * @author Tom
+ * @version 1.0
+ * @date 2021/2/17 21:14
+ */
+public class RefCountGC {
+    // 这个成员属性的唯一作用就是占用一点内存
+    private byte[] bigSize = new byte[5*1024*1024];
+    // 引用
+    Object reference = null;
+
+    public static void main(String[] args) {
+        RefCountGC obj1 = new RefCountGC();
+        RefCountGC obj2 = new RefCountGC();
+        obj1.reference = obj2;
+        obj2.reference = obj1;
+        obj1 = null;
+        obj2 = null;
+        // 显示的执行垃圾收集行为，判断obj1 和 obj2是否被回收？
+        System.gc();
+    }
+}
